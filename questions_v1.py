1600f6a05e85643fc88891ddf3a86ddb5d558456
greennsarahh <green.sarah@awatapu.school.nz>
2021-09-23 09:54:33 +1200

    if else and questions

---
+++ questions_v1.py
@@ -0,0 +1,41 @@
+# Ask user questions
+answer = int(input("How many legs does a spider have?"))
+if answer == 8:
+    print("Correct!")
+else:
+    print("Sorry that is incorrect")
+
+answer = int(input("How many planets are in our solar system?"))
+if answer == 8:
+    print("Correct!")
+else:
+    print("Sorry that is incorrect")
+
+answer = int(input("How many wings do bees have?"))
+if answer == 2:
+    print("Correct!")
+else:
+    print("Sorry that is incorrect")
+
+answer = int(input("How many days are in a year"))
+if answer == 365:
+    print("Correct!")
+else:
+    print("Sorry that is incorrect")
+
+answer = int(input("How many colours are in a rainbow?"))
+if answer == 7:
+    print("Correct!")
+else:
+    print("Sorry that is incorrect")
+
+answer = int(input("How many continents are there in the world?"))
+if answer == 7:
+    print("Correct!")
+else:
+    print("Sorry that is incorrect")
+
+
+
+
+
