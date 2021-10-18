98b3588d522748af87e14b247a28ad7282431f20
greennsarahh <green.sarah@awatapu.school.nz>
2021-10-18 13:23:25 +1300

    new

--- questions_v3_ML.py
+++ questions_v3_ML.py
@@ -1,24 +1,62 @@
 # variables
 
 # Ask user questions/ main routine
-def ask(question, answer):
-    error= "Please"
-    i = int(input(question))
-    if i == answer:
-        print("Correct!")
-        score += 1
-    else:
-        print("Sorry that is incorrect, the correct answer is {}.".format(answer))
+def ask(question):
+    error= "Please enter a whole number"
+    valid = False
+    while not valid
+        try:
+            response = int(input(question))
+            return response
+        except ValueError:
+            print error
+
 
 #** Main Routine ***
 score = 0
-
-q1 = ask("How many legs does a spider have?",8)
-q2 = ask("How many planets are in our solar system?", 8)
-q3 = ask("How many wings do bees have?", 2)
-q4 = ask("How many days are in a year", 365)
+q1 = ask("How many legs does a spider have?")
+if q1 == 8:
+    print("Correct!")
+    score +=1
+    print(score)
+else:
+    print("sorry that is incorrect, the correct answer is 8")
+q2 = ask("How many planets are in our solar system?")
+if q2 == 8:
+    print("Correct!")
+    score +=1
+    print(score)
+else:
+    print("sorry that is incorrect, the correct answer is 8")
+q3 = ask("How many wings do bees have?")
+if q3 == 2:
+    print("Correct!")
+    score +=1
+    print(score)
+else:
+    print("sorry that is incorrect, the correct answer is 2")
+q4 = ask("How many days are in a year")
+if q4 == 365:
+    print("Correct!")
+    score +=1
+    print(score)
+else:
+    print("sorry that is incorrect, the correct answer is 365")
 q5 = ask("How many colours are in a rainbow?", 7)
-q6 = ask("How many continents are there in the world?", 7)
+if q5 == 7:
+    print("Correct!")
+    score +=1
+    print(score)
+else:
+    print("sorry that is incorrect, the correct answer is 7")
+q6 = ask("How many continents are there in the world?")
+if q6 == 7:
+    print("Correct!")
+    score +=1
+    print(score)
+else:
+    print("sorry that is incorrect, the correct answer is 7")
+
 
 if score >= 6:
     print("You got {} out of 6. well done. ".format(score))
