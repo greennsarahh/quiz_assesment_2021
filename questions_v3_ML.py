7e31a1f51a89c5e0b7aea7964050c4181a8c0af3
greennsarahh <green.sarah@awatapu.school.nz>
2021-10-21 09:44:25 +1300

    nearly done

--- questions_v3_ML.py
+++ questions_v3_ML.py
@@ -4,12 +4,12 @@
 def ask(question):
     error= "Please enter a whole number"
     valid = False
-    while not valid
+    while not valid:
         try:
             response = int(input(question))
             return response
         except ValueError:
-            print error
+            print(error)
 
 
 #** Main Routine ***
@@ -18,42 +18,42 @@ q1 = ask("How many legs does a spider have?")
 if q1 == 8:
     print("Correct!")
     score +=1
-    print(score)
+    print("your current score is {}".format(score))
 else:
     print("sorry that is incorrect, the correct answer is 8")
 q2 = ask("How many planets are in our solar system?")
 if q2 == 8:
     print("Correct!")
     score +=1
-    print(score)
+    print("your current score is {}".format(score))
 else:
     print("sorry that is incorrect, the correct answer is 8")
 q3 = ask("How many wings do bees have?")
 if q3 == 2:
     print("Correct!")
     score +=1
-    print(score)
+    print("your current score is {}".format(score))
 else:
     print("sorry that is incorrect, the correct answer is 2")
 q4 = ask("How many days are in a year")
 if q4 == 365:
     print("Correct!")
     score +=1
-    print(score)
+    print("your current score is {}".format(score))
 else:
     print("sorry that is incorrect, the correct answer is 365")
-q5 = ask("How many colours are in a rainbow?", 7)
+q5 = ask("How many colours are in a rainbow?")
 if q5 == 7:
     print("Correct!")
     score +=1
-    print(score)
+    print("your current score is {}".format(score))
 else:
     print("sorry that is incorrect, the correct answer is 7")
 q6 = ask("How many continents are there in the world?")
 if q6 == 7:
     print("Correct!")
     score +=1
-    print(score)
+    print("your current score is {}".format(score))
 else:
     print("sorry that is incorrect, the correct answer is 7")
 
@@ -63,5 +63,6 @@ if score >= 6:
 elif score <= 5:
     print("You got {} out of 6. better luck next time. ".format(score))
 
+print("Thank you for playing")
 
 
